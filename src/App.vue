92e34e7ce8d8b086f31f8e4c0a29a035bcd40f25
Morten Hoffmann <moho@semler.dk>
2024-10-09 09:13:15 +0200

    minor

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,29 @@
         </p>
       </div>
       
+      <!-- carBrand -->
+      <div v-if="carBrandCheckbox">
+        <input id="carBrand" type="checkbox" v-model="carBrandCheckbox.checked" disabled />
+        <label for="carBrand">{{ carBrandCheckbox.label }}</label>
+
+        <transition>
+          <div v-if="carBrandCheckbox.radios && carBrandCheckbox.checked">
+            <div v-for="(radio, radioIndex) in carBrandCheckbox.radios" :key="'radio-' + radioIndex">
+              <input
+                :id="'radio-carBrand' + index + '-' + radioIndex"
+                type="radio"
+                :name="'radio-group-carBrand' + index"
+                :value="radio.selected"
+                v-model="carBrandCheckbox.selectedRadio"
+              />
+              <label :for="'radio-carBrand' + index + '-' + radioIndex">{{ radio.label }}</label>
+            </div>
+          </div>
+        </transition>
+      </div>
+
       <!-- carGroup -->
-      <div v-if="carGroupCheckbox">
+      <div v-if="carBrandCheckbox.selectedRadio != ''">
         <input id="carGroup" type="checkbox" v-model="carGroupCheckbox.checked" disabled />
         <label for="carGroup">{{ carGroupCheckbox.label }}</label>
 
@@ -114,30 +135,92 @@
         </transition>
       </div>
 
-      <!-- carFormat -->
-      <div v-if="carTypeGroupCheckbox.selectedRadio != '' || carTypeSpecificCheckbox.selectedRadio != ''">
-        <input id="carFormat" type="checkbox" v-model="carFormatCheckbox.checked" disabled />
-        <label for="carFormat">{{ carFormatCheckbox.label }}</label>
+      <!-- carFormatGroup -->
+      <div v-if="carBrandCheckbox.selectedRadio !== 'audi' && carGroupCheckbox.selectedRadio === 'group' && (carTypeGroupCheckbox.selectedRadio != '' || carTypeSpecificCheckbox.selectedRadio != '')">
+        <input id="carFormatGroup" type="checkbox" v-model="carFormatGroupCheckbox.checked" disabled />
+        <label for="carFormatGroup">{{ carFormatGroupCheckbox.label }}</label>
 
         <transition>
-          <div v-if="carFormatCheckbox.radios && carFormatCheckbox.checked">
-            <div v-for="(radio, radioIndex) in carFormatCheckbox.radios" :key="'radio-' + radioIndex">
+          <div v-if="carFormatGroupCheckbox.radios && carFormatGroupCheckbox.checked">
+            <div v-for="(radio, radioIndex) in carFormatGroupCheckbox.radios" :key="'radio-' + radioIndex">
               <input
-                :id="'radio-carFormat' + index + '-' + radioIndex"
+                :id="'radio-carFormatGroup' + index + '-' + radioIndex"
                 type="radio"
-                :name="'radio-group-carFormat' + index"
+                :name="'radio-group-carFormatGroup' + index"
                 :value="radio.selected"
-                v-model="carFormatCheckbox.selectedRadio"
+                v-model="carFormatGroupCheckbox.selectedRadio"
               />
-              <label :for="'radio-carFormat' + index + '-' + radioIndex">{{ radio.label }}</label>
+              <label :for="'radio-carFormatGroup' + index + '-' + radioIndex">{{ radio.label }}</label>
+            </div>
+          </div>
+        </transition>
+      </div>
+
+      <!-- carFormatGroupAudi -->
+      <div v-if="carBrandCheckbox.selectedRadio === 'audi' && carGroupCheckbox.selectedRadio === 'group' && (carTypeGroupCheckbox.selectedRadio != '' || carTypeSpecificCheckbox.selectedRadio != '')">
+        <input id="carFormatGroupAudi" type="checkbox" v-model="carFormatGroupAudiCheckbox.checked" disabled />
+        <label for="carFormatGroupAudi">{{ carFormatGroupAudiCheckbox.label }}</label>
+
+        <transition>
+          <div v-if="carFormatGroupAudiCheckbox.radios && carFormatGroupAudiCheckbox.checked">
+            <div v-for="(radio, radioIndex) in carFormatGroupAudiCheckbox.radios" :key="'radio-' + radioIndex">
+              <input
+                :id="'radio-carFormatGroupAudi' + index + '-' + radioIndex"
+                type="radio"
+                :name="'radio-group-carFormatGroupAudi' + index"
+                :value="radio.selected"
+                v-model="carFormatGroupAudiCheckbox.selectedRadio"
+              />
+              <label :for="'radio-carFormatGroupAudi' + index + '-' + radioIndex">{{ radio.label }}</label>
+            </div>
+          </div>
+        </transition>
+      </div>
+
+      <!-- carFormatSpecific -->
+      <div v-if="carBrandCheckbox.selectedRadio !== 'audi' && carGroupCheckbox.selectedRadio === 'specific' && (carTypeGroupCheckbox.selectedRadio != '' || carTypeSpecificCheckbox.selectedRadio != '')">
+        <input id="carFormatSpecific" type="checkbox" v-model="carFormatSpecificCheckbox.checked" disabled />
+        <label for="carFormatSpecific">{{ carFormatSpecificCheckbox.label }}</label>
+
+        <transition>
+          <div v-if="carFormatSpecificCheckbox.radios && carFormatSpecificCheckbox.checked">
+            <div v-for="(radio, radioIndex) in carFormatSpecificCheckbox.radios" :key="'radio-' + radioIndex">
+              <input
+                :id="'radio-carFormatSpecific' + index + '-' + radioIndex"
+                type="radio"
+                :name="'radio-group-carFormatSpecific' + index"
+                :value="radio.selected"
+                v-model="carFormatSpecificCheckbox.selectedRadio"
+              />
+              <label :for="'radio-carFormatSpecific' + index + '-' + radioIndex">{{ radio.label }}</label>
+            </div>
+          </div>
+        </transition>
+      </div>
+
+      <!-- carFormatSpecificAudi -->
+      <div v-if="carBrandCheckbox.selectedRadio === 'audi' && carGroupCheckbox.selectedRadio === 'specific' && (carTypeGroupCheckbox.selectedRadio != '' || carTypeSpecificCheckbox.selectedRadio != '')">
+        <input id="carFormatSpecificAudi" type="checkbox" v-model="carFormatSpecificAudiCheckbox.checked" disabled />
+        <label for="carFormatSpecificAudi">{{ carFormatSpecificAudiCheckbox.label }}</label>
+
+        <transition>
+          <div v-if="carFormatSpecificAudiCheckbox.radios && carFormatSpecificAudiCheckbox.checked">
+            <div v-for="(radio, radioIndex) in carFormatSpecificAudiCheckbox.radios" :key="'radio-' + radioIndex">
+              <input
+                :id="'radio-carFormatSpecificAudi' + index + '-' + radioIndex"
+                type="radio"
+                :name="'radio-group-carFormatSpecificAudi' + index"
+                :value="radio.selected"
+                v-model="carFormatSpecificAudiCheckbox.selectedRadio"
+              />
+              <label :for="'radio-carFormatSpecificAudi' + index + '-' + radioIndex">{{ radio.label }}</label>
             </div>
           </div>
         </transition>
       </div>
       
-      
       <!-- carStatus -->
-      <div v-if="carFormatCheckbox.selectedRadio != ''">
+      <div v-if="carFormatGroupCheckbox.selectedRadio != '' || carFormatSpecificCheckbox.selectedRadio != '' || carFormatGroupAudiCheckbox.selectedRadio != '' || carFormatSpecificAudiCheckbox.selectedRadio != ''">
         <input id="carStatus" type="checkbox" v-model="carStatusCheckbox.checked" disabled />
         <label for="carStatus">{{ carStatusCheckbox.label }}</label>
 
@@ -195,10 +278,11 @@
   
   <script>
 
-  import { ref, computed, onMounted } from 'vue';
+  import { ref, computed, onMounted, watch } from 'vue';
   
   export default {
     setup() {
+      const carBrandCheckbox = ref({});
       const carGroupCheckbox = ref({});
       
       const carImageGroupCheckbox = ref({});
@@ -207,7 +291,11 @@
       const carTypeGroupCheckbox = ref({});
       const carTypeSpecificCheckbox = ref({});
       
-      const carFormatCheckbox = ref({});
+      const carFormatGroupCheckbox = ref({});
+      const carFormatGroupAudiCheckbox = ref({});
+      const carFormatSpecificCheckbox = ref({});
+      const carFormatSpecificAudiCheckbox = ref({});
+
       const carStatusCheckbox = ref({});
 
       const carInsuranceCheckbox = ref({});
@@ -217,12 +305,16 @@
           const response = await fetch('../checkboxes.json');
           const data = await response.json();
 
+          carBrandCheckbox.value = data.carBrand;
           carGroupCheckbox.value = data.carGroup;
           carImageGroupCheckbox.value = data.carImageGroup;
           carImageSpecificCheckbox.value = data.carImageSpecific;
           carTypeGroupCheckbox.value = data.carTypeGroup;
           carTypeSpecificCheckbox.value = data.carTypeSpecific;
-          carFormatCheckbox.value = data.carFormat;
+          carFormatGroupCheckbox.value = data.carFormatGroup;
+          carFormatGroupAudiCheckbox.value = data.carFormatGroupAudi;
+          carFormatSpecificCheckbox.value = data.carFormatSpecific;
+          carFormatSpecificAudiCheckbox.value = data.carFormatSpecificAudi;
           carStatusCheckbox.value = data.carStatus;
           
           carInsuranceCheckbox.value = data.carInsurance;
@@ -231,6 +323,39 @@
           console.error('Error loading checkboxes:', error);
         }
       });
+
+    const resetDependentFields = () => {
+      carImageGroupCheckbox.value.selectedRadio = '';
+      carImageSpecificCheckbox.value.selectedRadio = '';
+      carTypeGroupCheckbox.value.selectedRadio = '';
+      carTypeSpecificCheckbox.value.selectedRadio = '';
+      carFormatGroupCheckbox.value.selectedRadio = '';
+      carFormatGroupAudiCheckbox.value.selectedRadio = '';
+      carFormatSpecificCheckbox.value.selectedRadio = '';
+      carFormatSpecificAudiCheckbox.value.selectedRadio = '';
+      carStatusCheckbox.value.selectedRadio = '';
+      carInsuranceCheckbox.value.selectedRadio = '';
+    };
+
+    // Watch for changes in carBrandCheckbox or carGroupCheckbox
+    watch(
+      () => carBrandCheckbox.value.selectedRadio,
+      (newValue, oldValue) => {
+        if (newValue !== oldValue && newValue !== undefined && newValue !== null) {
+          resetDependentFields();
+          carGroupCheckbox.value.selectedRadio = '';
+        }
+      }
+    );
+
+    watch(
+      () => carGroupCheckbox.value.selectedRadio,
+      (newValue, oldValue) => {
+        if (newValue !== oldValue && newValue !== undefined && newValue !== null) {
+          resetDependentFields();
+        }
+      }
+    );
 
       const combinedText = computed(() => {
 
@@ -256,8 +381,23 @@
           if (radio) { text += radio.value + ' '; }
         }
 
-        if (carFormatCheckbox.value.checked) {
-          const radio = carFormatCheckbox.value.radios.find(radio => radio.selected == carFormatCheckbox.value.selectedRadio);
+        if (carFormatGroupCheckbox.value.checked) {
+          const radio = carFormatGroupCheckbox.value.radios.find(radio => radio.selected == carFormatGroupCheckbox.value.selectedRadio);
+          if (radio) { text += radio.value + ' '; }
+        }
+
+        if (carFormatGroupAudiCheckbox.value.checked) {
+          const radio = carFormatGroupAudiCheckbox.value.radios.find(radio => radio.selected == carFormatGroupAudiCheckbox.value.selectedRadio);
+          if (radio) { text += radio.value + ' '; }
+        }
+
+        if (carFormatSpecificCheckbox.value.checked) {
+          const radio = carFormatSpecificCheckbox.value.radios.find(radio => radio.selected == carFormatSpecificCheckbox.value.selectedRadio);
+          if (radio) { text += radio.value + ' '; }
+        }
+
+        if (carFormatSpecificAudiCheckbox.value.checked) {
+          const radio = carFormatSpecificAudiCheckbox.value.radios.find(radio => radio.selected == carFormatSpecificAudiCheckbox.value.selectedRadio);
           if (radio) { text += radio.value + ' '; }
         }
 
@@ -275,12 +415,16 @@
       });
   
       return {
+        carBrandCheckbox,
         carGroupCheckbox,
         carImageGroupCheckbox,
         carImageSpecificCheckbox,
         carTypeGroupCheckbox,
         carTypeSpecificCheckbox,
-        carFormatCheckbox,
+        carFormatGroupCheckbox,
+        carFormatGroupAudiCheckbox,
+        carFormatSpecificCheckbox,
+        carFormatSpecificAudiCheckbox,
         carStatusCheckbox,
         carInsuranceCheckbox,
 
