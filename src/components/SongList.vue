20635af8f1cdee840534099bb9b959bf18e63693
Morten Hoffmann <moho@semler.dk>
2024-11-18 21:43:09 +0100

    inputmode

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -3,7 +3,7 @@
      <!-- <p>Add some songs!</p> -->
     <div class="songlist">
       <input class="song" v-model="newSong.name" placeholder="Song Name" />
-      <input class="bpm" v-model.number="newSong.bpm" type="number" placeholder="BPM" />
+      <input class="bpm" v-model.number="newSong.bpm" type="number" inputmode="tel" placeholder="BPM" />
       <button class="add" @click="addSong">
         <svg viewBox="0 0 24 24" width="32" height="32" stroke="white" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
         Tilføj
